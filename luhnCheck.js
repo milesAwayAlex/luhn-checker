f2ec3ffc8e200afa2d43e06fe8afef183b85f0ee
Alex M <milesAwayAlex@gmail.com>
2021-06-26 03:33:25 -0700

    Implement luhnCheck

--- luhnCheck.js
+++ luhnCheck.js
@@ -1,1 +1,14 @@
-export default () => {};
+export default number => {
+  const revNumArr = number
+    .toString()
+    .split('')
+    .reverse()
+    .map(e => +e);
+  const luhnSum = revNumArr
+    .map((e, i) => (i % 2 === 1 ? e * 2 : e))
+    .join('')
+    .split('')
+    .map(e => +e)
+    .reduce((a, e) => a + e);
+  return luhnSum % 10 === 0;
+};
